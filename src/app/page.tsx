e051658a402795ea8f127c948e1fe24c2a2ecab5
Ankush Kumar Gupta <167736782+ankush-coder-497001@users.noreply.github.com>
2026-05-30 00:13:28 +0530

    changed final changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default function HomePage() {
     <>
       <HeroBanner />
       <NewCollection />
-      <LimitedEdition />
+      {/* <LimitedEdition /> */}
 
       <StyleItWithFaoo />
       <WeCustomise />
